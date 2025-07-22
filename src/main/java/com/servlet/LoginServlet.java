01d79014281d0c00d1c70b895f8e474eaf2aeb22
Harsha Vardhan <140687832+SriHarsha1405@users.noreply.github.com>
2025-07-22 14:55:59 +0530

    Add files via upload

---
+++ src/main/java/com/servlet/LoginServlet.java
@@ -0,0 +1,42 @@
+package com.servlet;
+import com.dao.UserDao;
+import com.dao.UserDaoImpl;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static UserDao userDao = new UserDaoImpl();
+ 
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (userDao.isValidUser(username, password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            response.sendRedirect("welcome.jsp");
+            //System.out.println("Hi - "+username);
+        } else {
+            response.sendRedirect("login.jsp?error=1");
+        	//System.out.println("Error A gya");
+        }
+	}
+
+}
